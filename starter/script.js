7245d438a244f1a22c288d9d64604604198d3968
acparkgh <acpark123@gmail.com>
2021-05-22 02:52:26 -0400

    adding removing DOM elements

--- starter/script.js
+++ starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (event) {
+  event.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,12 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach((button) => {
+  return button.addEventListener("click", openModal);
+})
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +33,42 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+const header = document.querySelector(".header");
+// const allSections = document.querySelectorAll(".section")
+// console.log(allSections);
+
+// const sectionOne = document.querySelector("#section--1");
+// const sectionOne = document.getElementById("section--1");
+// console.log(sectionOne);
+
+// const allButtons = document.getElementsByTagName("button");
+// console.log(allButtons);
+
+// const allSections = document.getElementsByClassName("section");
+// console.log(allSections);
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.className = "cookie-message cookie-message1";
+message.id = "cookie"
+
+// message.textContent = "We use cookies for imporved functionality and analytics.";
+message.innerHTML = "We use cookies for imporved functionality and analytics. <button class='btn btn--close--cookie'>Got it!</button>";
+header.append(message);
+// header.after(message)
+
+const cookieButton = document.querySelector(".btn--close--cookie");
+cookieButton.addEventListener("click", function () {
+  // message.parentElement.removeChild(message);
+  message.remove();
 });
